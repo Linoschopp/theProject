ea4871de7b776b7cc8f698f5f63703708affea34
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-04-23 11:51:34 +0200

    Update server.py

--- theProject/server.py
+++ theProject/server.py
@@ -6,35 +6,46 @@ PORT = 8888
 ENCODING = "UTF-8"
 ADDR = (IP, PORT)
 
+clients = set()
+controller = ()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 server.listen()
 print("Listening")
 has_controller = False
+
+
 def handle_connection(conn, addr):
-	unsorted = True
-	while unsorted:
-		msg = conn.recv(64).decode(ENCODING)
-		if msg.startswith("type: "):
-			if msg == "type: controller":
-				if has_controller == False:
-					unsorted = False
-					handle_controller(conn, addr)a
-			else:
-				unsorted = False
-				handle_client(conn, addr)
-				
-				
-				
+    unsorted = True
+    while unsorted:
+        msg = conn.recv(64).decode(ENCODING)
+        if msg.startswith("type: "):
+            if msg == "type: controller":
+                if has_controller == False:
+                    unsorted = False
+                    handle_controller(conn, addr)
+            else:
+                unsorted = False
+                handle_client(conn, addr)
+
+
 def handle_controller(conn, addr):
-	raise NotImplementedError
-	
+    global controller
+    controller = (conn, addr)
+    connected = True
+    while connected:
+        data = conn.recv(1024).decode(ENCODING)
+    
+
 def handle_client(conn, addr):
-	raise NotImplementedError
-	
+    global clients
+    clients.add((conn, addr))
+
+
 running = True
 while running:
-	conn, addr = server.accept()
-	c = threading.Thread(target=handle_connection, args=(conn, addr))
-	c.start()
+    conn, addr = server.accept()
+    c = threading.Thread(target=handle_connection, args=(conn, addr))
+    c.start()
