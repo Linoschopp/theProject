a3f9b3bd2bbdb16ca0664bbf8d3e44bb46b65f71
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-04-24 13:38:49 +0200

    Update controller.py

--- theProject/controller.py
+++ theProject/controller.py
@@ -1,1 +1,27 @@
 #! /usr/bin/python3
+import socket
+
+
+IP = "127.0.0.1"
+PORT = 8888
+ENCODING = "UTF-8"
+ADDR = (IP, PORT)
+
+controller= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+while True:
+    try:
+        print("Trying to connect...")
+        controller.connect(ADDR)
+        break
+    except ConnectionRefusedError as e:
+        pass
+
+print("Connected")
+
+running = True
+active = False
+while running:
+     print("")
