039ce3f8e6aecc1272be20de9844006e18be039e
Linoschopp <143100072+Linoschopp@users.noreply.github.com>
2024-04-24 13:28:03 +0200

    client.py aktualisieren

---
+++ theProject/client.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import pyautogui
+
+IP = "127.0.0.1"
+PORT = 8888
+ENCODING = "UTF-8"
+ADDR = (IP, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def send_out(output):
+	client.send(f"tell lenght {len(output.encode(ENCODING))}".encode(ENCODING))
+	response = client.recv(1024)
+	if response == "tell ok":
+		client.send("tell content " + output.encode(ENCODING))
+
+while True:
+	try:
+		print("Trying to connect...")
+		client.connect(ADDR)
+		break
+	except ConnectionRefusedError as e:
+		pass
+
+print("Connected")
+
+running = True
+sys.stdout = send_out()
+while running:
+	data = client.recv(1024).decode(ENCODING)
+	if data.startswith("command length "):
+		try:
+			cmd_length = int(data[15:])
+		except ValueError as e:
+			conn.send("comand error".encode(ENCODING))
+			continue
+		client.send("command ok").encode(ENCODING)
